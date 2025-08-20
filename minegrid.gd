094a3ffba376dd4eb1fb550f00de0682301847f3
twig46 <tobyvdp@gmail.com>
2025-08-21 08:39:31 +1000

    Refactor minegrid and tile logic for tile interaction
    Replaces spawn_mines with spawn_grid in minegrid.gd, adds mine placement and first-click logic, and implements zero_spread for revealing adjacent tiles. Updates tile.gd to use atlas textures for tile states, connects pressed signal, and updates tile appearance based on index.z. These changes improve the grid's mine placement and tile interaction mechanics.

--- minegrid.gd
+++ minegrid.gd
@@ -3,24 +3,54 @@ extends GridContainer
 @export var gridsize:int = 3
 
 var example:TextureButton
+var first := true
+var visited := {}
 
 func _ready() -> void:
 	example = $tile
-	spawn_mines()
+	spawn_grid()
 
-func spawn_mines():
-	var index:= Vector3(0, 0, 2)
+func spawn_grid():
+	var index:= Vector3(0, 0, 0)
 	columns = gridsize
 	for i in range(pow(gridsize, 2)):
 		var newbutt = example.duplicate()
 		newbutt.index = index
+		if randi_range(1, 5) == 5:
+			newbutt.index.z = 13
 		add_child(newbutt)
 		index.x += 1
-		index.z = randi_range(0, 8)
 		if index.x >= gridsize:
 			index.y += 1
 			index.x = 0
 	example.queue_free()
 
-func clicked(buttindex: Vector3):
-	pass
+func clicked(buttindex: Vector3, tile: Control) -> void:
+	if first:
+		first = false
+		print(get_child_count())
+		for c in get_children():
+			if abs(c.index.x - buttindex.x) <= 1 and abs(c.index.y - buttindex.y) <= 1:
+				c.index.z = 0
+				c._on_pressed()
+				return
+	else:
+		for c in get_children():
+			if abs(c.index.x - buttindex.x) <= 1 and abs(c.index.y - buttindex.y) <= 1 and c.index.z == 13:
+				tile.index.z += 1
+				return
+	if buttindex.z == 0:
+		zero_spread(buttindex)
+		return
+
+
+func zero_spread(buttindex):
+	var key = str(buttindex.x) + ',' + str(buttindex.y)
+	if key in visited:
+		return
+	visited[key] = true
+	
+	for c in get_children():
+		if abs(c.index.x - buttindex.x) <= 1 and abs(c.index.y - buttindex.y) <= 1 and c.index.z == 0:
+			c._on_pressed()
+			return
--- tile.gd
+++ tile.gd
@@ -4,14 +4,17 @@ var index: Vector3 = Vector3.ZERO
 var one := preload("res://1.tres")
 
 func _ready() -> void:
-	var atlas = one.duplicate()
-	
-	texture_pressed = atlas
-	
-	var tempregion = texture_pressed
-	tempregion.region.position.x += (index.z)*17
-	
-	texture_pressed.region = tempregion.region
+	texture_normal = make_atlas(9 * 17)
 
-func _on_press() -> void:
-	get_parent().clicked(index)
+	texture_pressed = make_atlas(0)
+
+	pressed.connect(_on_pressed)
+
+func make_atlas(x: int) -> AtlasTexture:
+	var atlas: AtlasTexture = one.duplicate(true)
+	atlas.region = Rect2(Vector2(x, 49), atlas.region.size)
+	return atlas
+
+func _on_pressed() -> void:
+	await get_parent().clicked(index, self)
+	texture_normal = make_atlas(index.z * 17)
